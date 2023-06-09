549e530ee6dd4c8fda5b0e7f982ed107bb6706b9
11fwojcieszy <filip.wojcieszyn@sonova.com>
2023-06-09 14:13:52 +0200

    first commit

---
+++ csharp-host/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using uniffi.rust_lib;
+
+var code = RustLibMethods.GenerateQrCode("https://strathweb.com");
+Console.WriteLine(code);
---
+++ rust-lib/src/lib.rs
@@ -0,0 +1,50 @@
+uniffi::include_scaffolding!("rust-lib");
+
+use qrcodegen::{QrCode, QrCodeEcc};
+
+fn generate_qr_code(text: &str) -> String {
+	let errcorlvl: QrCodeEcc = QrCodeEcc::Low;  // Error correction level
+	
+	// Make and print the QR Code symbol
+	let qr: QrCode = QrCode::encode_text(text, errcorlvl).unwrap();
+	to_svg_string(&qr, 4)
+}
+
+fn to_svg_string(qr: &QrCode, border: i32) -> String {
+	assert!(border >= 0, "Border must be non-negative");
+	let mut result = String::new();
+	result += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	result += "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n";
+	let dimension = qr.size().checked_add(border.checked_mul(2).unwrap()).unwrap();
+	result += &format!(
+		"<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewBox=\"0 0 {0} {0}\" stroke=\"none\">\n", dimension);
+	result += "\t<rect width=\"100%\" height=\"100%\" fill=\"#FFFFFF\"/>\n";
+	result += "\t<path d=\"";
+	for y in 0 .. qr.size() {
+		for x in 0 .. qr.size() {
+			if qr.get_module(x, y) {
+				if x != 0 || y != 0 {
+					result += " ";
+				}
+				result += &format!("M{},{}h1v1h-1z", x + border, y + border);
+			}
+		}
+	}
+	result += "\" fill=\"#000000\"/>\n";
+	result += "</svg>\n";
+	result
+}
+
+
+// Prints the given QrCode object to the console.
+fn print_qr(qr: &QrCode) {
+	let border: i32 = 4;
+	for y in -border .. qr.size() + border {
+		for x in -border .. qr.size() + border {
+			let c: char = if qr.get_module(x, y) { '█' } else { ' ' };
+			print!("{0}{0}", c);
+		}
+		println!();
+	}
+	println!();
+}
